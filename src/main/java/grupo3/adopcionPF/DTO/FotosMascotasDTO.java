e990b8951961e44fd389a4aea89e4d4c59ca341d
safka18 <safkagallardo@gmail.com>
2020-11-11 23:01:03 -0300

    DTO

--- src/main/java/grupo3/adopcionPF/DTO/FotosMascotasDTO.java
+++ src/main/java/grupo3/adopcionPF/DTO/FotosMascotasDTO.java
@@ -1,11 +1,86 @@
 package grupo3.adopcionPF.DTO;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/api")
+
 public class FotosMascotasDTO {
-    private int id;
+    private int idFotosMascotas;
     private int mascota;
     private String foto1;
     private String foto2;
     private String foto3;
     private String foto4;
     private String foto5;
+
+    public FotosMascotasDTO(int idFotosMascotas, int mascota, String foto1, String foto2, String foto3, String foto4, String foto5) {
+        this.idFotosMascotas = idFotosMascotas;
+        this.mascota = mascota;
+        this.foto1 = foto1;
+        this.foto2 = foto2;
+        this.foto3 = foto3;
+        this.foto4 = foto4;
+        this.foto5 = foto5;
+    }
+
+    public int getIdFotosMascotas() {
+        return idFotosMascotas;
+    }
+
+    public void setIdFotosMascotas(int id) {
+        this.idFotosMascotas = idFotosMascotas;
+    }
+
+    public int getMascota() {
+        return mascota;
+    }
+
+    public void setMascota(int mascota) {
+        this.mascota = mascota;
+    }
+
+    public String getFoto1() {
+        return foto1;
+    }
+
+    public void setFoto1(String foto1) {
+        this.foto1 = foto1;
+    }
+
+    public String getFoto2() {
+        return foto2;
+    }
+
+    public void setFoto2(String foto2) {
+        this.foto2 = foto2;
+    }
+
+    public String getFoto3() {
+        return foto3;
+    }
+
+    public void setFoto3(String foto3) {
+        this.foto3 = foto3;
+    }
+
+    public String getFoto4() {
+        return foto4;
+    }
+
+    public void setFoto4(String foto4) {
+        this.foto4 = foto4;
+    }
+
+    public String getFoto5() {
+        return foto5;
+    }
+
+    public void setFoto5(String foto5) {
+        this.foto5 = foto5;
+    }
+
 }
--- src/main/java/grupo3/adopcionPF/DTO/MascotasDTO.java
+++ src/main/java/grupo3/adopcionPF/DTO/MascotasDTO.java
@@ -1,7 +1,9 @@
 package grupo3.adopcionPF.DTO;
 
+import java.util.Objects;
+
 public class MascotasDTO {
-    private int id;
+    private int idMascota;
     private String nombre;
     private int cuidador;
     private String especie;
@@ -9,5 +11,80 @@ public class MascotasDTO {
     private int edad;
     private String infoAdicional;
     private int fechaDePublicacion;
+
+    public MascotasDTO(int idMAscota, String nombre, int cuidador, String especie, String raza, int edad, String infoAdicional, int fechaDePublicacion) {
+        this.idMascota = idMAscota;
+        this.nombre = nombre;
+        this.cuidador = cuidador;
+        this.especie = especie;
+        this.raza = raza;
+        this.edad = edad;
+        this.infoAdicional = infoAdicional;
+        this.fechaDePublicacion = fechaDePublicacion;
+    }
+
+    public int getIdMascota() {
+        return idMascota;
+    }
+
+    public void setIdMascota(int idMascota) {
+        this.idMascota = idMascota;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCuidador() {
+        return cuidador;
+    }
+
+    public void setCuidador(int cuidador) {
+        this.cuidador = cuidador;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getInfoAdicional() {
+        return infoAdicional;
+    }
+
+    public void setInfoAdicional(String infoAdicional) {
+        this.infoAdicional = infoAdicional;
+    }
+
+    public int getFechaDePublicacion() {
+        return fechaDePublicacion;
+    }
+
+    public void setFechaDePublicacion(int fechaDePublicacion) {
+        this.fechaDePublicacion = fechaDePublicacion;
+    }
 
 }
