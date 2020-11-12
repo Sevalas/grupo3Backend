a5fd385b0436a0008d3b2b67d4feb30bd5bcf5d5
Sevalas <se.valas@outlook.com>
2020-11-11 23:10:18 -0300

    sorry

--- src/main/java/grupo3/adopcionPF/AdopcionPfApplication.java
+++ src/main/java/grupo3/adopcionPF/AdopcionPfApplication.java
@@ -2,14 +2,18 @@ package grupo3.adopcionPF;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
+
 @SpringBootApplication
+@EnableSwagger2
+@CrossOrigin(origins = "http://localhost:4200")
 public class AdopcionPfApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdopcionPfApplication.class, args);
 	}
-      //xsafka
+
+
 }
---
+++ src/main/java/grupo3/adopcionPF/resource/test.java
@@ -0,0 +1,9 @@
+package grupo3.adopcionPF.resource;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class test {
+    
+}
