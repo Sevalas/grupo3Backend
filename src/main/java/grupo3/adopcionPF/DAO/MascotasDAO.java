0013cd7cc11a83006f4017f2270d9d55d7b2ebad
Sevalas <se.valas@outlook.com>
2020-11-12 22:55:11 -0300

    1.0

--- src/main/java/grupo3/adopcionPF/DAO/MascotasDAO.java
+++ src/main/java/grupo3/adopcionPF/DAO/MascotasDAO.java
@@ -2,6 +2,7 @@ package grupo3.adopcionPF.DAO;
 
 import grupo3.adopcionPF.Conexion.connectionManager;
 import grupo3.adopcionPF.DTO.MascotasDTO;
+import grupo3.adopcionPF.DTO.UsuarioDTO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -56,5 +57,69 @@ public class MascotasDAO {
             } while (rs.next());
         }
         return listaMascotas;
+    }
+
+    public MascotasDTO obtenerMascotasPorId(int id) throws SQLException {
+        sql = "SELECT id,nombres,cuidador,especies,raza,edad,requisitos,fecha_de_publicacion " +
+                "FROM grupo3_mascotas " +
+                "WHERE id = ?";
+        ps = Conn.prepareStatement(sql);
+        ps.setInt(1, id);
+        rs = ps.executeQuery();
+        if (rs.next()) {
+            nuevoMascota= new MascotasDTO(rs.getInt("id"),
+                    rs.getString("nombres"),
+                    rs.getInt("cuidador"),
+                    rs.getString("especies"),
+                    rs.getString("raza"),
+                    rs.getInt("edad"),
+                    rs.getString("requisitos"),
+                    rs.getDate("fecha_de_publicacion"));
+        }
+        return nuevoMascota;
+    }
+
+    public MascotasDTO obtenerMascotasPorCuidador(int cuidador) throws SQLException {
+        sql = "SELECT id,nombres,cuidador,especies,raza,edad,requisitos,fecha_de_publicacion " +
+                "FROM grupo3_mascotas " +
+                "WHERE cuidador = ?";
+        ps = Conn.prepareStatement(sql);
+        ps.setInt(1, cuidador);
+        rs = ps.executeQuery();
+        if (rs.next()) {
+            nuevoMascota= new MascotasDTO(rs.getInt("id"),
+                    rs.getString("nombres"),
+                    rs.getInt("cuidador"),
+                    rs.getString("especies"),
+                    rs.getString("raza"),
+                    rs.getInt("edad"),
+                    rs.getString("requisitos"),
+                    rs.getDate("fecha_de_publicacion"));
+        }
+        return nuevoMascota;
+    }
+
+    public void actualizarMascotas(MascotasDTO mascota,int id) throws SQLException {
+        sql = "UPDATE grupo3_mascotas " +
+                "SET nombres=?,cuidador=?,especies=?,raza=?,edad=?,requisitos=?,fecha_de_publicacion=? " +
+                "WHERE id = ?";
+        ps = Conn.prepareStatement(sql);
+        ps.setString(1,mascota.getNombre());
+        ps.setInt(2,mascota.getCuidador());
+        ps.setString(3,mascota.getEspecie());
+        ps.setString(4,mascota.getRaza());
+        ps.setInt(5,mascota.getEdad());
+        ps.setString(6,mascota.getRequisitos());
+        ps.setDate(7,mascota.getFechaDePublicacion());
+        ps.setInt(8,id);
+        ps.execute();
+    }
+
+    public void eliminarMascota(int id)throws SQLException {
+        sql = "DELETE FROM grupo3_mascotas " +
+                "WHERE id = ?";
+        ps = Conn.prepareCall(sql);
+        ps.setInt(1,id);
+        ps.execute();
     }
 }
--- src/main/java/grupo3/adopcionPF/resources/MascotasAPI.java
+++ src/main/java/grupo3/adopcionPF/resources/MascotasAPI.java
@@ -17,8 +17,28 @@ public class MascotasAPI {
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/mascotas")
-    public List<MascotasDTO> obtenerListaUsuarios() throws SQLException {
+    public List<MascotasDTO> obtenerListaMascotas() throws SQLException {
         return new MascotasDAO().obtenerListaMascotas();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/mascotas/id={id}")
+    public MascotasDTO obtenerMascotaPorId(@PathVariable(name = "id") int id) throws SQLException {
+        return new MascotasDAO().obtenerMascotasPorId(id);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/mascotas/cuidador={cuidador}")
+    public MascotasDTO obtenerMascotaPorCuidador(@PathVariable(name = "cuidador") int cuidador) throws SQLException {
+        return new MascotasDAO().obtenerMascotasPorCuidador(cuidador);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT,value = "/mascotas/actualizar={id}")
+    public void actualizarMascota(@PathVariable("id") int id, @ModelAttribute MascotasDTO mascota) throws SQLException{
+        new MascotasDAO().actualizarMascotas(mascota,id);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/mascotas/eliminar={id}")
+    public void eliminarMascota(@PathVariable(name = "id") int id) throws SQLException {
+        new MascotasDAO().eliminarMascota(id);
     }
 
 }
